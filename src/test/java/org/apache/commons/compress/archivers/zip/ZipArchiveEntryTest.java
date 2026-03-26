0e8455f26cd7e80def664b4279268f18d053f1f7
Uday Shergill <udayshergill8@gmail.com>
2026-03-26 15:26:08 -0400

    test cases for ZipArchiveEntryTest

--- src/test/java/org/apache/commons/compress/archivers/zip/ZipArchiveEntryTest.java
+++ src/test/java/org/apache/commons/compress/archivers/zip/ZipArchiveEntryTest.java
@@ -19,8 +19,99 @@
 
 package org.apache.commons.compress.archivers.zip;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
 /**
  * Uday Shergill Testing ZipArchiveEntry
  */
 public class ZipArchiveEntryTest {
+
+    @Test
+    public void testSetAlignmentAcceptsDefaultZero() {
+        final ZipArchiveEntry entry = new ZipArchiveEntry("file.txt");
+
+        entry.setAlignment(0);
+
+        assertEquals(0, entry.getAlignment());
+    }
+
+    @Test
+    public void testSetAlignmentAcceptsPowerOfTwoWithinLimit() {
+        final ZipArchiveEntry entry = new ZipArchiveEntry("file.txt");
+
+        entry.setAlignment(32_768);
+
+        assertEquals(32_768, entry.getAlignment());
+    }
+
+    @Test
+    public void testSetAlignmentRejectsNonPowerOfTwo() {
+        final ZipArchiveEntry entry = new ZipArchiveEntry("file.txt");
+
+        try {
+            entry.setAlignment(3);
+            fail("Expected IllegalArgumentException");
+        } catch (final IllegalArgumentException expected) {
+            // expected
+        }
+    }
+
+    @Test
+    public void testSetAlignmentRejectsValueAboveLimit() {
+        final ZipArchiveEntry entry = new ZipArchiveEntry("file.txt");
+
+        try {
+            entry.setAlignment(65_536);
+            fail("Expected IllegalArgumentException");
+        } catch (final IllegalArgumentException expected) {
+            // expected
+        }
+    }
+
+    @Test
+    public void testSetUnixModeSetsUnixPlatformAndModeForRegularFile() {
+        final ZipArchiveEntry entry = new ZipArchiveEntry("file.txt");
+
+        entry.setUnixMode(0755);
+
+        assertEquals(ZipArchiveEntry.PLATFORM_UNIX, entry.getPlatform());
+        assertEquals(0755, entry.getUnixMode());
+        assertEquals(0755L << 16, entry.getExternalAttributes());
+    }
+
+    @Test
+    public void testSetUnixModeSetsReadOnlyFlagWhenOwnerWriteMissing() {
+        final ZipArchiveEntry entry = new ZipArchiveEntry("file.txt");
+
+        entry.setUnixMode(0555);
+
+        assertEquals(ZipArchiveEntry.PLATFORM_UNIX, entry.getPlatform());
+        assertEquals(0555, entry.getUnixMode());
+        assertEquals((0555L << 16) | 1L, entry.getExternalAttributes());
+    }
+
+    @Test
+    public void testSetUnixModeSetsDirectoryFlagForDirectoryEntries() {
+        final ZipArchiveEntry entry = new ZipArchiveEntry("folder/");
+
+        entry.setUnixMode(0755);
+
+        assertEquals(ZipArchiveEntry.PLATFORM_UNIX, entry.getPlatform());
+        assertEquals(0755, entry.getUnixMode());
+        assertEquals((0755L << 16) | 0x10L, entry.getExternalAttributes());
+    }
+
+    @Test
+    public void testSetUnixModeSetsBothDirectoryAndReadOnlyFlagsWhenNeeded() {
+        final ZipArchiveEntry entry = new ZipArchiveEntry("folder/");
+
+        entry.setUnixMode(0555);
+
+        assertEquals(ZipArchiveEntry.PLATFORM_UNIX, entry.getPlatform());
+        assertEquals(0555, entry.getUnixMode());
+        assertEquals((0555L << 16) | 0x10L | 1L, entry.getExternalAttributes());
+    }
 }
