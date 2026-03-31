45c3bf8dc74606dbb36538d45e25c2e3c3d7a3ad
Ben-Corbet <bencorbet01@gmail.com>
2026-03-30 20:50:14 -0400

    added unimplemented test file

---
+++ src/test/java/org/apache/commons/compress/archivers/zip/ZipFileTest.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.apache.commons.compress.archivers.zip;
+
+/**
+ *
+ * @author jjapp
+ */
+public class ZipFileTest {
+    
+}
